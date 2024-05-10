06be60c5ccb5f4213093f77ccecc466f274b94dc
Mahedi Hasan <mahedi.hasan@brainstation-23.com>
2024-05-10 19:16:44 +0600

    top movie model class is update

--- lib/feature/home/home_screen.dart
+++ lib/feature/home/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:movie_app/feature/home/model/top_movie.dart';
+import 'package:movie_app/feature/home/model/top_movie_model.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
